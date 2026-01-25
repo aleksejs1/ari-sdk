7d75364ada220ee03d645e38f0a52af7bbb65b17
Aleksejs Kovalovs <aleks4444@inbox.lv>
2026-01-25 16:31:27 +0200

    Fix sdk

--- src/build/test.ts
+++ src/build/test.ts
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { fileURLToPath } from 'url';
+import { fileURLToPath, pathToFileURL } from 'url';
 
 export function createTestConfig(importerUrl: string) {
     const root = path.dirname(fileURLToPath(importerUrl));
 
     return defineConfig({
         plugins: [react()],
+        resolve: {
+            alias: {
+                '@': path.resolve(root, './src').replace(/\\/g, '/'),
+                'react': path.resolve(root, '../../../node_modules/react').replace(/\\/g, '/'),
+                'react-dom': path.resolve(root, '../../../node_modules/react-dom').replace(/\\/g, '/'),
+                'react-hook-form': path.resolve(root, '../../../node_modules/react-hook-form').replace(/\\/g, '/'),
+            },
+        },
         test: {
             root,
             globals: true,
@@ -15,9 +23,19 @@ export function createTestConfig(importerUrl: string) {
             include: ['src/**/*.test.ts', 'src/**/*.test.tsx'],
             setupFiles: ['@ari/plugin-sdk/test/setup'],
             css: false,
-            alias: {
-                '@': path.resolve(root, './src'),
+            server: {
+                deps: {
+                    external: ['react', 'react-dom'],
+                },
             },
         },
+        server: {
+            fs: {
+                allow: [
+                    root,
+                    path.resolve(root, '../../../../../')
+                ]
+            }
+        }
     });
 }
