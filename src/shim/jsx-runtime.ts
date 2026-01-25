f65a54c227a3515f00ac9f5f6fd720c51065eb72
Aleksejs Kovalovs <aleks4444@inbox.lv>
2026-01-25 05:10:23 +0200

    Optimize web plugin deps

--- src/shim/jsx-runtime.ts
+++
@@ -1,6 +0,0 @@
-const ReactJSX = (window as any).ReactJSX;
-
-export const Fragment = ReactJSX.Fragment;
-export const jsx = ReactJSX.jsx;
-export const jsxs = ReactJSX.jsxs;
-export const jsxDEV = ReactJSX.jsxDEV;
